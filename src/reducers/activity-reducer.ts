46d6916d885d9c1b28fcb5e5f0d2df627499c8cf
AlfonsoVidrio <154586838+AlfonsoVidrio@users.noreply.github.com>
2026-05-29 02:17:21 -0600

    feat: implement activity reducer with CRUD actions

---
+++ src/reducers/activity-reducer.ts
@@ -0,0 +1,62 @@
+import type { Activity } from "../types"
+
+export type ActivityActions = 
+    {type: 'save-activity', payload: { newActivity: Activity }}  |
+    {type: 'set-activeId', payload: { id: Activity['id'] }} |
+    {type: 'delete-activity', payload: { id: Activity['id'] }} |
+    {type: 'delete-all'}
+
+export type ActivityState = {
+    activities: Activity[]
+    activeId: Activity['id']
+}
+
+const localStorageActivities = () : Activity[] => {
+    const activities = localStorage.getItem('activities')
+    return activities ? JSON.parse(activities) : []
+}
+
+export const initialState: ActivityState = {
+    activities: localStorageActivities(),
+    activeId: ''
+}
+
+export const activityReducer = (state: ActivityState, action: ActivityActions) => {
+    switch(action.type) {
+        case 'save-activity': {
+            let updateActivities: Activity[] = []
+            
+            if (state.activeId) {
+                updateActivities = state.activities.map( activity => activity.id === state.activeId ? action.payload.newActivity : activity)
+            } else {
+                updateActivities = [...state.activities, action.payload.newActivity]
+            }
+
+            return {
+                ...state,
+                activities: updateActivities,
+                activeId: ''
+            }
+        }
+        case 'set-activeId': {
+            return {
+                ...state,
+                activeId: action.payload.id
+            }
+        }
+        case 'delete-activity': {
+            return {
+                ...state,
+                activities: state.activities.filter( activity => activity.id !== action.payload.id )
+            }
+        }
+        case 'delete-all': {
+            return {
+                activities: [],
+                activeId: ''
+            }
+        }
+        default:
+            return state
+    }
+}
